647a8af0a949317c9f6eb598b9f4f7d5989d48cd
p1ck23 <danil-vetrov47@mail.ru>
2024-03-11 21:05:23 +0400

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import PopQuitAccount from "./components/PopQuitAccount";
 import PopNewCard from "./components/PopNewCard";
 import "./App.css";
 import PopBrowser from "./components/PopBrowse";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -10,52 +11,8 @@ function App() {
         <PopQuitAccount />
         <PopNewCard />
         <PopBrowser />
-
-        <header className="header">
-          <div className="container">
-            <div className="header__block">
-              <div className="header__logo _show _light">
-                <a href="" target="_self">
-                  <img src="images/logo.png" alt="logo" />
-                </a>
-              </div>
-              <div className="header__logo _dark">
-                <a href="" target="_self">
-                  <img src="images/logo_dark.png" alt="logo" />
-                </a>
-              </div>
-              <nav className="header__nav">
-                <button
-                  className="header__btn-main-new _hover01"
-                  id="btnMainNew"
-                >
-                  <a href="#popNewCard">Создать новую задачу</a>
-                </button>
-                <a href="#user-set-target" className="header__user _hover02">
-                  Ivan Ivanov
-                </a>
-                <div
-                  className="header__pop-user-set pop-user-set"
-                  id="user-set-target"
-                >
-                  <p className="pop-user-set__name">Ivan Ivanov</p>
-                  <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                  <div className="pop-user-set__theme">
-                    <p>Темная тема</p>
-                    <input
-                      type="checkbox"
-                      className="checkbox"
-                      name="checkbox"
-                    />
-                  </div>
-                  <button type="button" className="_hover03">
-                    <a href="#popExit">Выйти</a>
-                  </button>
-                </div>
-              </nav>
-            </div>
-          </div>
-        </header>
+        <Header/>
+        
         <main className="main">
           <div className="container">
             <div className="main__block">
