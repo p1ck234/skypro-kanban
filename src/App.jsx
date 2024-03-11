68a98015c72142c9fb9f5e4db0e0a785b85e6281
p1ck23 <danil-vetrov47@mail.ru>
2024-03-11 20:57:29 +0400

    PopQuitAccount

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import PopQuitAccount from "./components/PopQuitAccount";
 import "./App.css";
 
 function App() {
@@ -9,25 +10,7 @@ function App() {
   return (
     <>
       <div class="wrapper">
-        <div class="pop-exit" id="popExit">
-          <div class="pop-exit__container">
-            <div class="pop-exit__block">
-              <div class="pop-exit__ttl">
-                <h2>Выйти из аккаунта?</h2>
-              </div>
-              <form class="pop-exit__form" id="formExit" action="#">
-                <div class="pop-exit__form-group">
-                  <button class="pop-exit__exit-yes _hover01" id="exitYes">
-                    <a href="modal/signin.html">Да, выйти</a>{" "}
-                  </button>
-                  <button class="pop-exit__exit-no _hover03" id="exitNo">
-                    <a href="main.html">Нет, остаться</a>{" "}
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
+        <PopQuitAccount />
 
         <div class="pop-new-card" id="popNewCard">
           <div class="pop-new-card__container">
