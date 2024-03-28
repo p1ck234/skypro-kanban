d25b2dd50c4e59bb5d449f50bf1d3738257a5463
p1ck23 <danil-vetrov47@mail.ru>
2024-03-28 22:22:58 +0400

    Header full styled

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,30 @@
+import { Container } from "../../styles/shared";
 import PopDrowdown from "../popups/PopDropdown";
 import * as S from "./Header.styled";
 
 function Header({ onCardAdd }) {
   return (
     <S.Header>
-      <div className="container">
-        <div className="header__block">
-          <div className="header__logo _show _light">
+      <Container>
+        <S.HeaderBlock>
+          <S.HeaderLogo>
             <a href="" target="_self">
               <img src="/logo.png" alt="logo" />
             </a>
-          </div>
-          <div className="header__logo _dark">
+          </S.HeaderLogo>
+          {/* <div className="header__logo _dark">
             <a href="" target="_self">
               <img src="images/logo_dark.png" alt="logo" />
             </a>
-          </div>
-          <nav className="header__nav">
+          </div> */}
+          <S.HeaderNav>
             <S.HeaderBtn onClick={onCardAdd}>
               <a href="#">Создать новую задачу</a>
             </S.HeaderBtn>
             <PopDrowdown onCardAdd={onCardAdd} />
-          </nav>
-        </div>
-      </div>
+          </S.HeaderNav>
+        </S.HeaderBlock>
+      </Container>
     </S.Header>
   );
 }
--- src/styles/shared.js
+++ src/styles/shared.js
@@ -1,7 +1,14 @@
-import { css } from "styled-components"
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1260px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 30px;
+`;
 
 export const Hover01 = css`
-&:hover {
+  &:hover {
     background-color: #33399b;
-}
+  }
 `;
