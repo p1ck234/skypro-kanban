43568652b2ef4054cde4d76301bfb8ca733cf694
p1ck23 <danil-vetrov47@mail.ru>
2024-04-04 20:24:29 +0400

    card page and exit page

---
+++ src/pages/CardPage.jsx
@@ -0,0 +1,7 @@
+import Card from "../components/card/Card";
+
+const CardPage = () => {
+  return <Card></Card>;
+};
+
+export default CardPage;
---
+++ src/pages/ExitPage.jsx
@@ -0,0 +1,7 @@
+import PopQuitAccount from "../components/popups/PopQuitAccount/PopQuitAccount";
+
+const ExitPage = () => {
+  return <PopQuitAccount></PopQuitAccount>;
+};
+
+export default ExitPage;
