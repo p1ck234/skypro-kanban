7a37b195d8d91d2d86f2543ac4369ae0bc3e7020
p1ck23 <danil-vetrov47@mail.ru>
2024-04-20 06:47:51 +0400

    add style calendar

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,6 +1,7 @@
 import { DayPicker } from "react-day-picker";
 import { format } from "date-fns";
 import { ru } from "date-fns/locale";
+import "react-day-picker/dist/style.css"
 
 function Calendar({ selected, setSelected }) {
   let footer = <p>Выберите срок исполнения.</p>;
